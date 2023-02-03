b29aff0eede751fca8a22e701a3708ba46829c94
myropikypri <124047848+myropikypri@users.noreply.github.com>
2023-02-03 20:03:04 +0200

    Update game-code.js

--- js/game-code.js
+++ js/game-code.js
@@ -361,7 +361,7 @@
 		*/
 		function preload() 
 		{
-			background = loadImage("game-assets/moon-bg.jpg");		// load the background-image
+			background = loadImage("game-assets/poli2.jpg");		// load the background-image
 			asteroidImage = loadImage("game-assets/asteroid-2.png"); // load once and the pass to Asteroid so that will not load each time an Asteroid is created
 			spaceship = new SpaceShip();
 			
